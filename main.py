b039ff5aac1b9b66e1a45d6ddbf790ff5910e750
Konoe <20051031jason@gmail.com>
2026-05-23 00:28:18 +0800

    Map

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import pygame
 import map
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((960, 720))
 clock = pygame.time.Clock()
 running = True
+
+x = y = 0
 
 while running:
 	for event in pygame.event.get():
 		# User press 'X'
 		if event.type == pygame.QUIT:
 			running = False
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:	x -= .25
+			if event.key == pygame.K_RIGHT:	x += .25
+			if event.key == pygame.K_UP:	y -= .25
+			if event.key == pygame.K_DOWN:	y += .25
+			print(x,y)
 	# Game
-	map.draw_background(screen)
+	map.draw_background(screen, (x, y))
 	# Display
 	pygame.display.flip()
 	clock.tick(60)
--- map.py
+++ map.py
@@ -1,6 +1,33 @@
-import typing
-if typing.TYPE_CHECKING:
-	import pygame
+import pygame
+import random
+import math
+from typing import Tuple
 
-def draw_background(screen: pygame.Surface):
-	pass
+background_size = 48
+background_source = dict[str, list[pygame.Surface]]()
+background_source_dict = { "grass": 1, "ocean": 1 }
+for type, count in background_source_dict.items():
+	background_source[type] = [
+		pygame.transform.scale(
+			pygame.image.load(f"./texture/{type}{i}.jpg"),
+			(background_size, background_size)
+		) for i in range(count)
+	]
+def get_background_tile(x: int, y: int) -> pygame.Surface:
+	random.seed(f"({x},{y})")
+	rand = random.randint(1, background_source_dict["grass"]) - 1
+	return background_source["grass"][rand]
+
+def draw_background(screen: pygame.Surface, player_position: Tuple[float, float]):
+	tile = background_source["grass"][0]
+	print(player_position)
+	px = player_position[0] * background_size - screen.get_width() / 2
+	py = player_position[1] * background_size - screen.get_height() / 2
+	dx = px - math.ceil(px / background_size) * background_size
+	while dx < screen.get_width():
+		dy = py - math.ceil(py / background_size) * background_size
+		while dy < screen.get_height():
+			tile = get_background_tile((px + dx) // background_size, (py + dy) // background_size)
+			screen.blit(tile, (dx, dy))
+			dy += tile.get_height()
+		dx += tile.get_width()
